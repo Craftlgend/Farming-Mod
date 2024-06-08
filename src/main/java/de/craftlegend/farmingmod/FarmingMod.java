e5bd92471173b1cbd308a4ddb3db5b2cf121f69e
Craftlgend <constantin.rasch@yahoo.com>
2024-06-08 14:37:40 -0600

    Added Item and Item Group

--- src/main/java/de/craftlegend/farmingmod/FarmingMod.java
+++ src/main/java/de/craftlegend/farmingmod/FarmingMod.java
@@ -5,13 +5,16 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import de.craftlegend.farmingmod.item.ModItemGroups;
+import de.craftlegend.farmingmod.item.ModItems;
+
 public class FarmingMod implements ModInitializer {
 	public static final String MOD_ID = "farming-mod";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-
-		LOGGER.info("Hello Fabric world!");
+		ModItemGroups.registerItemGroups();
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/de/craftlegend/farmingmod/item/ModItemGroups.java
@@ -0,0 +1,24 @@
+package de.craftlegend.farmingmod.item;
+
+import de.craftlegend.farmingmod.FarmingMod;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class ModItemGroups {
+    public static final ItemGroup FARMING_GROUP = Registry.register(Registries.ITEM_GROUP,
+            new Identifier(FarmingMod.MOD_ID, "ruby"),
+            FabricItemGroup.builder().displayName(Text.translatable("itemgroup.ruby"))
+                    .icon(() -> new ItemStack(ModItems.RUBY)).entries((displayContext, entries) -> {
+                        entries.add(ModItems.RUBY);
+                    }).build());
+
+
+    public static void registerItemGroups(){
+        FarmingMod.LOGGER.info("Registering Item Groups for"+FarmingMod.MOD_ID);
+    }
+}
